9d318cc8cb710f5857404c5b9d056bcec7dde0f7
Makoto Inoue <inouemak@googlemail.com>
2012-03-29 14:34:26 +0100

    Added drbs

---
+++ origin/drbs.rb
@@ -0,0 +1,14 @@
+require './drb_origin.rb'
+a = {}
+ODRb.start_service('druby://localhost:7640', a)
+loop do
+  sleep 1
+  p a
+end
+ODRb.thread.join
+
+# Usage
+# require './drb_origin'
+# ODRb.start_service('druby://localhost:7640', {})
+# o = ODRbObject.new(nil, 'druby://localhost:7640')
+# o["hello"] = "world"
